1bde1d8ebffbb5d031527224469c7ac46452ff3a
Benjamin <Benjamin@BENJIPC>
2016-10-05 17:17:02 -0500

    Add project files.

---
+++ UNACH.TallerXamarin.Core/Model/Usario.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNACH.TallerXamarin.Core.Model
+{
+    public class Usuario : ObservableObject
+    {
+
+        private string nombre;
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                Set(ref nombre, value);
+            }
+        }
+    }
+}
---
+++ UNACH.TallerXamarin.Core/ViewModel/SaludoViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UNACH.TallerXamarin.Core.Model;
+
+namespace UNACH.TallerXamarin.Core.ViewModel
+{
+    public class SaludoViewModel : ViewModelBase
+    {
+        public SaludoViewModel()
+        {
+            Usuario.PropertyChanged += (s, e) =>
+            {
+                switch (e.PropertyName)
+                {
+                    case "Nombre":
+                        SaludarCommand.RaiseCanExecuteChanged();
+                        break;
+                    default:
+                        break;
+                }
+            };
+        }
+        
+        private Usuario usuario;
+        public Usuario Usuario
+        {
+            get { return usuario ?? (usuario = new Usuario()); }
+            set { Set(ref usuario, value); }
+        }
+        
+        private string saludo;
+        public string Saludo
+        {
+            get { return saludo; }
+            set { Set(ref saludo, value); }
+        }
+
+        RelayCommand saludarCommand = null;
+        public RelayCommand SaludarCommand
+        {
+            get
+            {
+                return saludarCommand ?? (saludarCommand = new RelayCommand(() =>
+                {
+                    Saludo = $"Hola {Usuario.Nombre} {DateTime.Now.ToString()}";
+                }, 
+                () => { return !string.IsNullOrWhiteSpace(Usuario.Nombre); }));
+            }
+        }
+    }
+}
